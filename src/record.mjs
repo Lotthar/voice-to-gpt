cde8a8a035d845867f53d445dff99a39f375b8ff
Branko Rovcanin <branko.rovcanin@gmail.com>
2023-03-26 23:39:10 +0200

    Trying out conversions

--- src/record.mjs
+++ src/record.mjs
@@ -1,10 +1,13 @@
 import prism from "prism-media";
 import { EndBehaviorType } from "@discordjs/voice";
+import { StreamEncoder } from "flac-bindings";
 import { pipeline } from "node:stream";
-import { createWriteStream, readFileSync } from "node:fs";
+import { createWriteStream, createReadStream } from "node:fs";
 import { playOpenAiAnswerAfterSpeech } from "./audio-text.mjs";
 import { processAudioStreamIntoText } from "./google-speech-to-text.mjs";
 import path from "path";
+import pkg from "@discordjs/opus";
+const { OpusEncoder } = pkg;
 
 let isCurrentlyProcessing = false;
 
@@ -13,8 +16,20 @@ export const createOggFileForProcessing = async (connection, userId) => {
   if (isCurrentlyProcessing) return;
   isCurrentlyProcessing = true;
   const opusStream = getOpusStream(connection, userId);
-  const oggStream = getOggStream();
-  pipelineOpusStreamIntoFile(connection, opusStream, oggStream, oggFilePath);
+  // const oggStream = getOggStream();
+  const flacFilePath = path.join("./recordings", `test.flac`);
+
+  const flacFile = opusStream
+    // TODO: Swap decoder with any other possible NE RADI PRSIM ENCODER/DECODER na novim OPUS fajlovima
+    //  izgleda da je problem velicina buffer-a
+    // Probati OpusDecored iz discord opus paketa
+    .pipe(new prism.opus.Decoder({ rate: 48000, channels: 2, frameSize: 960 }))
+    .pipe(getFlacEncoder())
+    .pipe(createWriteStream(flacFilePath));
+  flacFile.on("finish", () => {
+    console.log("Successfully created .flac audio file!");
+    readFlacFileAndPlayAnswer(connection, flacFilePath);
+  });
 };
 
 const pipelineOpusStreamIntoFile = (connection, opusStream, oggStream, oggFilePath) => {
@@ -30,10 +45,8 @@ const pipelineOpusStreamIntoFile = (connection, opusStream, oggStream, oggFilePa
   });
 };
 
-const readOpusFileAndPlayAnswer = async (connection, oggFilePath) => {
-  // TODO: ovdje odraditi konverziju .ogg to .flac ako ke uopste moguce
-  const flacFilePath = path.join("./recordings", `sample.flac`);
-  const audioFile = readFileSync(flacFilePath);
+const readFlacFileAndPlayAnswer = async (connection, flacFilePath) => {
+  const audioFile = path.join("./recordings", `test.flac`);
   await playOpenAiAnswerAfterSpeech(
     connection,
     audioFile.toString("base64"),
@@ -41,13 +54,16 @@ const readOpusFileAndPlayAnswer = async (connection, oggFilePath) => {
   );
 };
 
-/**
- * Getting raw audio from opus stream with prism Decoder
- *
- * @param {*} opusStream - given opus stream to process
- */
-export const getOpusDecoder = (opusStream) => {
-  return new prism.opus.Decoder({ frameSize: 960, channels: 2, rate: 48000 });
+const getFlacEncoder = () => {
+  // // Create a new FLAC encoder instance
+  return new StreamEncoder({
+    compressionLevel: 5, // Set the compression level (0-8)
+    totalSamples: -1, // Set the total number of samples (-1 for unknown)
+    bitsPerSample: 16, // Set the number of bits per sample
+    sampleRate: 48000, // Set the sample rate
+    channels: 2, // Set the number of channels
+    verify: false, // Disable verification
+  });
 };
 
 /**
