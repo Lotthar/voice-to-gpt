fc7d3b15791cef253dcd68259d844ad0bf28817c
Branko Rovcanin <branko.rovcanin@gmail.com>
2023-06-03 20:53:51 +0200

    Updated voices

--- src/interfaces/voice.ts
+++ src/interfaces/voice.ts
@@ -9,15 +9,16 @@ export const DEFAULT_ENGLISH_VOICE = "Google";
 export const voices = [
   DEFAULT_ENGLISH_VOICE,
   "Morgan Freeman",
+  "Sir David Attenborough (New)",
+  "Samuel L. Jackson (New)",
   "Snoop Dogg (V2)",
   "Rick Sanchez",
   "Optimus Prime (Peter Cullen)",
-  "Morty Smith",
+  "Morty Smith (Version 2.0)",
   "The Joker (Heath Ledger, Version 2.0)",
   "Eminem (Slim Shady era - 1997 - 2001)",
   "James Earl Jones",
   "Sean Connery",
-  "2Pac (Tupac Amaru Shakur) (ARPAbet supported)",
 ];
 
 export let currentVoice: SpeechVoice = {
