f6eae69d09cb56f51ff601b7213b50242c7241db
Branko Rovcanin <branko.rovcanin@gmail.com>
2023-11-30 08:40:14 +0100

    extract converters to global

--- src/util/audio-util.ts
+++ src/util/audio-util.ts
@@ -3,13 +3,15 @@ import { convertOpusStreamToWavBuffer } from "./stream-util.js";
 import { AudioReceiveStream } from "@discordjs/voice";
 import wav from "wav";
 
+const opusEncoder = new opus.OpusEncoder(48000, 2);
+
+const wavEncoder= new wav.Writer({
+  channels: 2,
+  sampleRate: 48000,
+  bitDepth: 16,
+});
+
 export const createWavAudioBufferFromOpus = async (opusStream: AudioReceiveStream, channelId: string): Promise<Buffer> => {
-  const opusEncoder = new opus.OpusEncoder(48000, 2);
-  const wavEncoder= new wav.Writer({
-    channels: 2,
-    sampleRate: 48000,
-    bitDepth: 16,
-  });
   try {
     return await convertOpusStreamToWavBuffer(opusStream,opusEncoder,wavEncoder);
   } catch (error) {
