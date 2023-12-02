e2b4b7db44585281b63b1b95afb9bb0e773f32c2
Branko Rovcanin <branko.rovcanin@gmail.com>
2023-12-02 22:33:45 +0100

    add waiting sound

--- src/types/discord.ts
+++ src/types/discord.ts
@@ -1,3 +1,5 @@
 import { TextChannel, VoiceChannel } from "discord.js";
 
 export type ChannelCommonType = TextChannel | VoiceChannel | null;
+
+export const waitingAudioURI = "https://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg";
--- src/util/discord-voice.ts
+++ src/util/discord-voice.ts
@@ -1,23 +1,29 @@
+import axios from "axios";
 import { generateOpenAIAnswer } from "../openai/openai-api.js";
 import { generateSpeechFromText, generateTextFromSpeech } from "../openai/openai-whisper-api.js";
 import { sendMessageToProperChannel } from "./discord-util.js";
-import { createAudioResource, createAudioPlayer, AudioPlayer, VoiceConnection, AudioPlayerStatus } from "@discordjs/voice";
+import { createAudioResource, createAudioPlayer, AudioPlayer, VoiceConnection, AudioPlayerStatus, StreamType, AudioResource } from "@discordjs/voice";
+import { Readable } from "node:stream";
+import { waitingAudioURI } from "../types/discord.js";
 
 let player: AudioPlayer | null = null;
+let waitingAudioResource: AudioResource<null> | undefined;
 
 export const playOpenAiAnswerWithSpeech= async (audioBuffer: Buffer, connection: VoiceConnection, channelId: string) => {
-  initAndSubscribeAudioPlayerToVoiceChannel(connection);
+  await initAndSubscribeAudioPlayerToVoiceChannel(connection);
   const transcript = await generateTextFromSpeech(audioBuffer, "wav");
   const openAiAnswer = await generateOpenAIAnswer(transcript!, channelId);
   await playSpeechAudioFromText(openAiAnswer, channelId);
 };
 
-const initAndSubscribeAudioPlayerToVoiceChannel = (connection: VoiceConnection): void => {
+const initAndSubscribeAudioPlayerToVoiceChannel = async (connection: VoiceConnection): Promise<void> => {
   if(player === null) {
     player = createAudioPlayer();
     connection.subscribe(player!);
     addOnErrorPlayerEvent();
   }
+  await generateWaitingAudioResourceIfNone(); 
+  if(!!waitingAudioResource) player!.play(waitingAudioResource);
 };
 
 const playSpeechAudioFromText = async (text: string | null, channelId: string): Promise<void> => {
@@ -33,3 +39,19 @@ const addOnErrorPlayerEvent = (): void => {
     console.error("Error:", error.message, "with audio", error.resource.metadata.title);
   });
 };
+
+const generateWaitingAudioResourceIfNone = async() => {
+  if(!waitingAudioResource) {
+    try {
+      const { data } = await axios.get(waitingAudioURI, {
+        responseType: 'arraybuffer',
+        headers: {
+            'Content-Type': 'audio/ogg'
+        }
+      });
+      waitingAudioResource = createAudioResource(Readable.from(data));
+    } catch(error) {
+      console.error("Error creating waiting sound before the answer!", error);
+    }
+  }
+}
